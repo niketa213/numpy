deb55253d6b2a7e86c094d1ad34e0cacfc1cb7d9
@niketa213 <niketaniketa07@gmail.com>
2025-03-19 11:48:52 +0530

    new code

---
+++ code.py
@@ -0,0 +1,26 @@
+#indexing =excess in array a specific element and specific position 
+#slicing =exess multiple element in array {contaion (start,stop,step)}
+#fancing making=contain specific condition
+import numpy as np
+# arr=np.array([11,30,40,50,60,20,70,80])
+###INDEXING
+## print(arr[1]) #30
+## print(arr[5]) #20
+## print(arr[7])#80
+
+###SLICING
+## print(arr[1:6:1])
+## print(arr[1:5:2]) #30,40
+## print(arr[::2])
+## print(arr[::-1])
+## print(arr[2:7]) 
+
+#FANCING
+#print(arr[[0,2,4]]) #11,40,60
+#print(arr[[2,4,7]])
+
+#FILTERING
+## print(arr[arr > 20])
+## print(arr[arr >= 10])
+## print(arr[arr != 30])
+##print(arr[arr <= 50])
